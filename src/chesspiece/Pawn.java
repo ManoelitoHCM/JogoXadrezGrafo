3f962f863649eb5a93075e60fbb922d6996b9f73
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-10 15:24:43 -0300

    Inicio da implementação de movimentação do peão

--- src/chesspiece/Pawn.java
+++ src/chesspiece/Pawn.java
@@ -1,5 +1,6 @@
 package chesspiece;
 
+import board.ChessGraph;
 import board.ChessNode;
 
 public class Pawn extends ChessPiece {
@@ -11,6 +12,24 @@ public class Pawn extends ChessPiece {
 
     @Override
     public ChessNode[][] getPossibleMoves(ChessNode[][] board) {
+
+        int row = getCurrentNode().getRow();
+        int col = getCurrentNode().getCol();
+
+        // para alinhar a movimentação de acordo com o lado do tabuleiro
+        int direction = (getColor() == Color.WHITE) ? -1 : 1;
+
+        // um movimento para frente e um para cada diagonal
+        ChessNode[][] possibleMoves = new ChessNode[3][1];
+        int moveCount = 0;
+
+        ChessNode nextNode = new ChessNode(row + direction, col);
+
+        // verifica a frente
+        if (ChessGraph.getGraphInstance().isValidPosition(nextNode) && !ChessGraph) {
+
+        }
+
         return new ChessNode[0][];
     }
 }
