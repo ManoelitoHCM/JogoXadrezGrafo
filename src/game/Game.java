684265ff3cc3f042a068baf743b5060d01302576
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-12 20:01:05 -0300

    teste main

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,6 +1,7 @@
 package game;
 
 import board.ChessGraph;
+import board.ChessNode;
 import chesspiece.*;
 
 public class Game {
@@ -13,20 +14,17 @@ public class Game {
         chessBoard.display();
 
         // Testa alguns movimentos possíveis para uma peça (por exemplo, um cavalo)
-        ChessPiece knight = new Knight(Color.WHITE, chessBoard.getNode(0, 1));
-
+        ChessPiece knight = chessBoard.getNode(0, 1).getPiece();
         System.out.println("Movimentos possíveis para o cavalo na posição (0, 1):");
-        knight.filterValidMoves().forEach(System.out::println);
+        knight.filterValidMoves(chessBoard.getNode(0, 1)).forEach(System.out::println);
         knight.move(chessBoard.getNode(2, 2));
         chessBoard.display();
-        knight.filterValidMoves().forEach(System.out::println);
-
+        knight.filterValidMoves(chessBoard.getNode(2, 2)).forEach(System.out::println);
 
         // Testa alguns movimentos possíveis para outra peça (por exemplo, uma torre)
-        ChessPiece rook = new Rook(Color.BLACK, chessBoard.getNode(7, 7));
-        System.out.println("Movimentos possíveis para a torre na posição (7, 7):");
-        rook.filterValidMoves().forEach(System.out::println);
+        //ChessPiece rook = chessBoard.getNode(7, 7).getPiece();
+        //System.out.println("Movimentos possíveis para a torre na posição (7, 7):");
+        //rook.filterValidMoves(chessBoard.getNode(7, 7)).forEach(System.out::println);
 
         // Implemente mais testes conforme necessário
-    }
-}
+    }}
