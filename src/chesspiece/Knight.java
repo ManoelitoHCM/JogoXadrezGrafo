6cd348c31de350bcb701c332867d1082a50991b9
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-12 20:00:52 -0300

    ajuste do retorno da função offset

--- src/chesspiece/Knight.java
+++ src/chesspiece/Knight.java
@@ -2,9 +2,6 @@ package chesspiece;
 
 import board.ChessNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Knight extends ChessPiece {
 
     public Knight(Color color, ChessNode currentNode) {
@@ -12,26 +9,12 @@ public class Knight extends ChessPiece {
     }
 
     @Override
-    public List<ChessNode> getPossibleMoves() {
-
-        List<ChessNode> possibleMoves = new ArrayList<>();
-        ChessNode currentPosition = getCurrentNode();
-
-        int[][] moveOffsets = {
+    public int[][] getOffsets() {
+        return new int[][]{
                 {-2, -1}, {-2, 1}, // movimento em L para frente
                 {-1, -2}, {-1, 2}, // movimento em L para os lados
                 {1, -2}, {1, 2},
                 {2, -1}, {2, 1}    // movimento em L para trás
         };
-
-        for (int[] offset : moveOffsets) {
-
-            ChessNode newNode = new ChessNode(currentPosition.getRow() + offset[0], currentPosition.getCol() + offset[1]);
-
-            if (isValidMove(newNode)) {
-                possibleMoves.add(newNode);
-            }
-        }
-        return possibleMoves;
     }
 }
