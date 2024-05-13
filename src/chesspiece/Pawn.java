019975c67d3beea861b7c252f91496a4b5c79521
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-12 23:00:44 -0300

    @super move

--- src/chesspiece/Pawn.java
+++ src/chesspiece/Pawn.java
@@ -4,7 +4,7 @@ import board.ChessNode;
 
 public class Pawn extends ChessPiece {
 
-    private boolean hasMoved;
+    private boolean hasMoved = false;
 
     public Pawn(Color color, ChessNode currentNode) {
         super(color, currentNode);
@@ -14,16 +14,22 @@ public class Pawn extends ChessPiece {
     public int[][] getOffsets() {
         if (getColor() == Color.WHITE) {
             if (!hasMoved) {
-                return new int[][]{{-2, 0}, {-1, 0}, {-1, 1}, {-1, -1}};
-            } else {
-                return new int[][]{{-1, 0}, {-1, 1}, {-1, -1}};
-            }
-        } else {
-            if (!hasMoved) {
                 return new int[][]{{2, 0}, {1, 0}, {1, 1}, {1, -1}};
             } else {
                 return new int[][]{{1, 0}, {1, 1}, {1, -1}};
             }
+        } else {
+            if (!hasMoved) {
+                return new int[][]{{-2, 0}, {-1, 0}, {-1, 1}, {-1, -1}};
+            } else {
+                return new int[][]{{-1, 0}, {-1, 1}, {-1, -1}};
+            }
         }
+    }
+
+    @Override
+    public void move(ChessNode newNode) {
+        super.move(newNode);
+        hasMoved = true;
     }
 }
