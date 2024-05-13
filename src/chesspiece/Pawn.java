789a3faec3dc930d94ae306db98aaca0615e4184
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-13 10:42:39 -0300

    adição de lógica para detectar peça oponente na diagonal

--- src/chesspiece/Pawn.java
+++ src/chesspiece/Pawn.java
@@ -12,19 +12,42 @@ public class Pawn extends ChessPiece {
 
     @Override
     public int[][] getOffsets() {
-        if (getColor() == Color.WHITE) {
-            if (!hasMoved) {
-                return new int[][]{{2, 0}, {1, 0}, {1, 1}, {1, -1}};
+        if (!isOpponentInDiagonal()) {
+            if (getColor() == Color.WHITE) {
+                if (!hasMoved) {
+                    return new int[][]{{2, 0}, {1, 0}};
+                } else {
+                    return new int[][]{{1, 0}};
+                }
             } else {
-                return new int[][]{{1, 0}, {1, 1}, {1, -1}};
+                if (!hasMoved) {
+                    return new int[][]{{-2, 0}, {-1, 0}};
+                } else {
+                    return new int[][]{{-1, 0}};
+                }
             }
         } else {
-            if (!hasMoved) {
-                return new int[][]{{-2, 0}, {-1, 0}, {-1, 1}, {-1, -1}};
+            if (getColor() == Color.WHITE) {
+                return new int[][]{{1, 1}, {1, -1}};
             } else {
-                return new int[][]{{-1, 0}, {-1, 1}, {-1, -1}};
+                return new int[][]{{-1, 1}, {-1, -1}};
             }
         }
+    }
+
+    private boolean isOpponentInDiagonal() {
+        for (ChessNode neighbor : this.getCurrentNode().getNeighbors()) {
+            if (neighbor.hasPiece() && neighbor.getPiece().isOpponentPiece(this.getCurrentNode())) ;
+            {
+                int rowDifference = neighbor.getRow() - this.getCurrentNode().getRow();
+                int colDifference = neighbor.getCol() - this.getCurrentNode().getCol();
+                // Se a peça estiver na diagonal
+                if (Math.abs(rowDifference) == 1 && Math.abs(colDifference) == 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
