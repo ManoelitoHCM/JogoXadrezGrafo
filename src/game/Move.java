bace2057ea77b9edcc3e2c9a465ce3384d6249df
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-14 11:02:27 -0300

    inclusão da lógica de movimentaçáo de uma peça no tabuleiro em uma classe designada para reduzir as responsabilidades da classe Game

--- src/game/Move.java
+++ src/game/Move.java
@@ -1,4 +1,43 @@
 package game;
 
+import board.ChessNode;
+import chesspiece.ChessPiece;
+import exceptions.InvalidMoveException;
+import exceptions.NoPieceOnNodeException;
+import exceptions.NotPlayersTurnException;
+
 public class Move {
+    private ChessNode fromNode;
+    private ChessNode toNode;
+    private ChessPiece piece;
+
+    public Move(ChessNode fromNode, ChessNode toNode) {
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+        this.piece = fromNode.getPiece();
+    }
+
+    public boolean isValid() {
+        return fromNode != null && toNode != null && fromNode.hasPiece() && piece.isValidMove(toNode);
+    }
+
+    public void execute() throws NoPieceOnNodeException, NotPlayersTurnException, InvalidMoveException {
+        if (fromNode == null || !fromNode.hasPiece()) {
+            throw new NoPieceOnNodeException("Não há peça na posição (" + fromNode.getRow() + ", " + fromNode.getCol() + ").");
+        }
+
+        if (piece == null) {
+            throw new InvalidMoveException("Nenhuma peça para mover.");
+        }
+
+        if (!isValid()) {
+            throw new InvalidMoveException("Movimento inválido de " + piece.getClass().getSimpleName() + " para a posição (" + toNode.getRow() + ", " + toNode.getCol() + ").");
+        }
+
+        piece.move(toNode);
+    }
+
+    public ChessPiece getPiece() {
+        return piece;
+    }
 }
