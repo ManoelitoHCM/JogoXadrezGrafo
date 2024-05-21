c3746e112d988be0aae3cadedeadd4ad47ef379a
Manoelito Holanda <107510463+ManoelitoHCM@users.noreply.github.com>
2024-05-20 22:22:10 -0300

    ajuste com método findKing

--- src/search/DepthFirstSearch.java
+++ src/search/DepthFirstSearch.java
@@ -2,6 +2,7 @@ package search;
 
 import board.ChessNode;
 import chesspiece.ChessPiece;
+import chesspiece.King;
 
 import java.util.*;
 
@@ -36,7 +37,6 @@ public class DepthFirstSearch {
                         int newCol = piece.getCurrentNode().getCol() + offset[1];
 
                         if (currentNode.getRow() == newRow && currentNode.getCol() == newCol) {
-                            System.out.println("comparou e adicionou");
                             possibleMoves.add(currentNode);
                         }
                     }
@@ -47,5 +47,45 @@ public class DepthFirstSearch {
             throw new NullPointerException("Não há peça nessa posição.");
         }
     }
+
+    public static ChessNode findOppositeKing(ChessNode startNode) {
+
+        ChessNode kingNode = null;
+        Set<ChessNode> visitedNodes = new HashSet<>();
+        Stack<ChessNode> stack = new Stack<>();
+
+        if (startNode.hasPiece()) {
+
+            visitedNodes.add(startNode);
+            stack.push(startNode);
+
+            while (!stack.isEmpty()) {
+                ChessNode currentNode = stack.pop();
+
+                for (ChessNode neighbor : currentNode.getNeighbors()) {
+                    if (!visitedNodes.contains(neighbor)) {
+                        visitedNodes.add(neighbor);
+                        stack.push(neighbor);
+                    }
+                }
+
+                if (currentNode.hasPiece() &&
+                        currentNode.getPiece().isOpponentPiece(startNode) &&
+                        currentNode.getPiece() instanceof King) {
+
+                    kingNode = currentNode;
+                    break;
+                }
+            }
+            if (kingNode != null) {
+                return kingNode;
+            } else {
+                throw new NoSuchElementException("Rei do adversário não encontrado no tabuleiro.");
+            }
+        } else {
+            throw new NullPointerException("Não há peça na posição inicial.");
+        }
+    }
+
 
 }
